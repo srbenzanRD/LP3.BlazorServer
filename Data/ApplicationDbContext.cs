25221269f8027743bde2e09a12dcc8c466a12bee
Felix R. Benzan <felixbenz07@gmail.com>
2026-05-25 19:33:36 -0400

    Agregando entidades de Dominio en el contexto de la base de datos

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,8 +1,19 @@
+using LP3.BlazorServer.Domain.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace LP3.BlazorServer.Data;
 
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+public class ApplicationDbContext
+(DbContextOptions<ApplicationDbContext> options) : 
+IdentityDbContext<ApplicationUser>(options)
 {
+    public DbSet<Estudiante> Estudiantes => Set<Estudiante>();
+    public DbSet<Curso> Cursos => Set<Curso>();
+    public DbSet<Matriculacion> Matriculaciones => Set<Matriculacion>();
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        return base.SaveChangesAsync(cancellationToken);
+    }
 }
